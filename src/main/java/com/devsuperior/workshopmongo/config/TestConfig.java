ea4e79f54a118633adcf7ce79af8aeac03609e32
Andre Landim <andrelandim_4@hotmail.com>
2022-04-15 00:31:22 -0300

    Inserting User

---
+++ src/main/java/com/devsuperior/workshopmongo/config/TestConfig.java
@@ -0,0 +1,31 @@
+package com.devsuperior.workshopmongo.config;
+
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.devsuperior.workshopmongo.models.entities.User;
+import com.devsuperior.workshopmongo.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostConstruct
+	public void init() {
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Gonçalves", "maria@gmail.com");
+		User alex = new User(null, "Alex Sampaio", "alex@gmail.com");
+		User davi = new User(null, "Davi Germano", "davi@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, davi));
+	}
+}
