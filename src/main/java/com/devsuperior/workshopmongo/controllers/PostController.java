2f1b268c9daeb4cfad3764451dd94de471d8d77b
Andre Landim <andrelandim_4@hotmail.com>
2022-04-16 19:08:44 -0300

    GET Post by id, query methods

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,10 +1,13 @@
 package com.devsuperior.workshopmongo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.workshopmongo.models.dto.PostDTO;
@@ -22,4 +25,12 @@ public class PostController {
 		PostDTO obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<PostDTO>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
+		List<PostDTO> obj = service.findByTitle(text);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
 }
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,8 +23,16 @@ public class PostService {
 		return new PostDTO(entity);
 	}
 	
+	public List<PostDTO> findByTitle(String text){
+		List<Post> list = repository.findByTitleContainingIgnoreCase(text);
+		return list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
+		
+	}
+	
 	private Post getEntityById(String id) {
 		Optional<Post> result = repository.findById(id);
 		return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
 	}
+	
+	
 }
