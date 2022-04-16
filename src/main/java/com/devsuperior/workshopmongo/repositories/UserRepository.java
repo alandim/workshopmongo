816ec4e58b5265730c3396909ebe01a4b7a16390
Andre Landim <andrelandim_4@hotmail.com>
2022-04-15 22:36:26 -0300

    GET user by id

--- src/main/java/com/devsuperior/workshopmongo/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/workshopmongo/repositories/UserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import com.devsuperior.workshopmongo.models.entities.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, String> {
+
+	
 
 }
